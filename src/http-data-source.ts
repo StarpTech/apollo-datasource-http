d4ab8152deb5a70f3c2ec0b286e074463a6cd0e4
Kamil Dybicz <kamil@secretescapes.com>
2022-06-30 15:46:25 +0200

    making sure local caching options override global ones

--- src/http-data-source.ts
+++ src/http-data-source.ts
@@ -425,8 +425,8 @@ export abstract class HTTPDataSource<TContext = any> extends DataSource {
     }
 
     const options = {
-      ...request,
       ...this.globalRequestOptions,
+      ...request,
       headers,
     }
 
